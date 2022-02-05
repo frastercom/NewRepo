ae71fa7c6b06d5b8044ef7457645c9155c75dd85
Konyshev Timofey <frastercom@gmail.com>
2022-02-05 08:33:27 +0300

    new file math

---
+++ NewRepo/Math.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Math
+{
+public:int sum(int p, string d, int p2);
+	  int sub(int p, string d, int p2);
+	  int mul(int p, string d, int p2);
+	  int div(int p, string d, int p2);
+};
+
