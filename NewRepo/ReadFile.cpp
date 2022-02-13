788e4bdb91f7f3a979a4952f0b0bdf6913f28760
parzival-2077 <89209738+parzival-2077@users.noreply.github.com>
2022-02-13 15:04:41 +0300

    изменения в записи в файл ReadFile

--- NewRepo/ReadFile.cpp
+++ NewRepo/ReadFile.cpp
@@ -21,21 +21,18 @@ vector<string> ReadFile::readDocument(string name) {
 	return readDocument;
 }
 void ReadFile::writeDocument(string name, string data) {
-	setlocale(LC_ALL, "rus");
-	vector <string> readDocument;
-	ifstream f(name);
-	if (f.is_open()) {
-		cout << "OK" << endl;
-		string  data;
+	ofstream file(name);
+	if (!file) {
+		cout << "error" << endl;
+		return 0;
 
-		while (!f.eof()) {
-			f >> data;
-			cout << data << endl;
-			readDocument.push_back(data);
-		}
 	}
-	else cout << "File no open" << endl;
-	f.close();
+	
+	for (int i : data) {
+		file << i << endl;
+	}
+
+	file.close();
 
 	
 }
