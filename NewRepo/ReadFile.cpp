d60287527aa84a9a213cc3f77f3488ab905b0054
Курбатов Алексей <kuratot7453@gmail.com>
2022-02-13 13:28:14 +0300

    прописан файл cpp для ReadFile.h

--- NewRepo/ReadFile.cpp
+++ NewRepo/ReadFile.cpp
@@ -1,1 +1,26 @@
 #include "ReadFile.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+void writeDocument(string name, string data) {
+	setlocale(LC_ALL, "rus");
+	vector <string> readDocument;
+	ifstream f(name);
+	if (f.is_open()) {
+		cout << "OK" << endl;
+		string  data;
+
+		while (!f.eof()) {
+			f >> data;
+			cout << data << endl;
+			readDocument.push_back(data);
+		}
+	}
+	else cout << "File no open" << endl;
+	f.close();
+
+	
+}
